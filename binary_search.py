1f98b885d5799eac79011cbffa09cb8872534109
bishal9874 <bm8130377@gmail.com>
2022-01-18 15:09:21 +0530

    matrix Sum

--- binary_search.py
+++ binary_search.py
@@ -1,33 +1,19 @@
-
-
-
 def binarySearch(arr, l, r, x):
 
  
     if r >= l:
-
-        mid = l + (r - l) // 2
-
-        
+        mid = l + (r - l) // 2      
         if arr[mid] == x:
             return mid
-
         elif arr[mid] > x:
-            return binarySearch(arr, l, mid-1, x)
-
-       
+            return binarySearch(arr, l, mid-1, x)      
         else:
             return binarySearch(arr, mid + 1, r, x)
-
-    else:
-       
+    else:       
         return -1
 
-
-
 arr = [2, 3, 4, 10, 40]
-x = 10
-
+x = int(input("enter the number to found: "))
 
 result = binarySearch(arr, 0, len(arr)-1, x)
 
